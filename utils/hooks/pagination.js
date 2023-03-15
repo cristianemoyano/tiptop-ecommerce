1b75f9298f05b1f92d8ce6657f0cf4d86e458c4a
cmoyano <cristianmoyano.mza@gmail.com>
2023-03-15 10:14:03 -0300

    change page size

--- utils/hooks/pagination.js
+++ utils/hooks/pagination.js
@@ -2,7 +2,7 @@ import { db } from '../../services/firebase-config';
 
 import { query, getDocs, collection, limit, startAfter, orderBy } from 'firebase/firestore';
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 12;
 
 
 const getProductsPaginated = (lastDocument, onGetProducts) => {
